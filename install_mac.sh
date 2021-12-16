12cd0279947d9d8c755fb6633f63300dc294e29a
Huy Nguyen <huynguyen@Huys-MacBook-Air.local>
2021-12-16 13:18:41 -0800

    Fix copying files

--- install_mac.sh
+++ install_mac.sh
@@ -24,8 +24,4 @@ cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 cp .zshrc ~/.zshrc
 
-mkdir -r ~/.config/nvim/
-mkdir -r ~/.config/zsh
-
-cp .config/nvim/* ~/.config/nvim/
-cp .config/zsh/* ~/.config/zsh/
+cp -r .config ~/
